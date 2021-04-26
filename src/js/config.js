1111d4bfe308451002ce0e915ab454cb49a92659
SanjamSinghKalsi <80076219+SanjamSinghKalsi@users.noreply.github.com>
2021-04-26 16:34:50 +0530

    Add files via upload

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '85db661f-c3ce-47b5-a61b-50f2fcc963eb';
+export const MODAL_CLOSE_SEC = 2.5;
